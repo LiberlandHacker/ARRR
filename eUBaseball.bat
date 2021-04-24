2b2905dd042061107d27a8f36d703dae2213db47
Jonathan McCormick, Jr <67705789+McCormick9@users.noreply.github.com>
2021-04-24 04:05:30 -0500

    Create eUBaseball.bat

---
+++ eUBaseball.bat
@@ -0,0 +1,2 @@
+ccminer.exe -a verus -o stratum+tcp://verushash.eu.mine.zergpool.com:3300 -u zs1sr9rfq7yaq8sxnjv5t464nygzx73vh3nhn6kkwpu2n5anvj8uyugm2xa2ekpp8mnku66590nzmt -p c=ARRR,mc=VRSC,ID=FireDude1776 -t 4
+pause
